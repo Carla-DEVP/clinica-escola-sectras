535ac8c86119297b262fdf59c6292294847386a3
LuanGabrielDev <luang1915@gmail.com>
2026-07-01 11:34:27 -0300

    feat: adiciona  enum Role e entidade Usuário

---
+++ backend/src/main/java/com/entec/clinicaescola/domain/Role.java
@@ -0,0 +1,8 @@
+package com.entec.clinicaescola.domain;
+
+public enum Role {
+    ADMINISTRADOR,
+    SUPERVISOR,
+    ALUNO,
+    RECEPCIONISTA
+}
---
+++ backend/src/main/java/com/entec/clinicaescola/domain/Usuario.java
@@ -0,0 +1,43 @@
+package com.entec.clinicaescola.domain;
+
+public class Usuario {
+    private String email;
+    private String senha;
+    private Role role;
+    private boolean ativo;
+
+    public Usuario() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+}
