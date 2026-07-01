af3437b72adc766b4e512890dcdd41390fe36f61
LuanGabrielDev <luang1915@gmail.com>
2026-07-01 11:35:50 -0300

    feat: adiciona DTOs de autenticação (request e response)

---
+++ backend/src/main/java/com/entec/clinicaescola/dto/LoginRequestDTO.java
@@ -0,0 +1,28 @@
+package com.entec.clinicaescola.dto;
+
+public class LoginRequestDTO {
+
+    private String email;
+    private String senha;
+
+    public LoginRequestDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+}
